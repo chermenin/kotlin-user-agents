50f998edc1b9d6c89b7f4d07c869a85068615dde
Alex Chermenin <alex@chermenin.ru>
2022-02-03 09:38:23 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.4.10"
     maven
     signing
+    `maven-publish`
 }
 
 group = "ru.chermenin"
@@ -95,4 +96,22 @@ tasks.named<Upload>("uploadArchives") {
 
 signing {
     sign(configurations.archives.get())
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
